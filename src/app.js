94a54076ed5be0d86538afd3a7eb6e7dfbf2fed9
fniclcg <lichunguang@fnic.cn>
2019-01-04 17:13:16 +0800

    update
    update

--- src/app.js
+++ src/app.js
@@ -155,7 +155,8 @@ App({
   },
   globalData:{
     userInfo:{
-      islogin:false
+      islogin:false,
+      tt: "test"
     }
   }
 })
--- src/pages/billboard/index.js
+++ src/pages/billboard/index.js
@@ -381,7 +381,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log("[onReady globalData] => ", app.globalData);
   },
 
   /**
